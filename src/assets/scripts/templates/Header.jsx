51fae7761907f8b1a9e450b3efdbf42b6fcfad1e
mercaton <m.wieczorek001@wp.pl>
2023-04-24 22:28:08 +0200

    add html structure to header section

--- src/assets/scripts/templates/Header.jsx
+++ src/assets/scripts/templates/Header.jsx
@@ -1,7 +1,26 @@
 
 function Header() {
     return (
-        <h1>Hello World</h1>
+        <header className="header">
+            <div className="header__wrapper">
+                <div className="header__menu">
+                    <ul>
+                        <a href=""><li className="menu-item">O mnie</li></a>
+                        <a href=""><li className="menu-item">Oferta</li></a>
+                        <a href=""><li className="menu-item">Blog</li></a>
+                        <a href=""><li className="menu-item">Kontakt</li></a>
+
+                    </ul>
+                </div>
+                <div className="header__logo">
+                    <h1 className="logo-text">Empathy Horsewoman</h1>
+                </div>
+                <div className="header__socials">
+                    <a href="" className="fb-icon">fb</a>
+                    <a href="" className="insta-icon">insta</a>
+                </div>
+            </div>
+        </header>
     );
 }
 
