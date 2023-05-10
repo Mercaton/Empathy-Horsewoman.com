2afc7777773a083130e37a885923c4fc74230fc2
mercaton <m.wieczorek001@wp.pl>
2023-05-10 20:33:33 +0200

    adding rwd to aboutme section

--- src/assets/scripts/templates/Aboutme.jsx
+++ src/assets/scripts/templates/Aboutme.jsx
@@ -7,12 +7,10 @@ function Aboutme() {
                 <div className="aboutme-bg"></div>
                 <div className="aboutme-txt">
                     <h1 className="aboutme-hdl">ABOUT ME</h1>
-                    <span className="text1">Empatia, współpraca, tolerancja, akceptacja, nowoczesność i nauka.</span>
-                    <span className="text2">Jestem trenerem i coachem. Wspieram konie i ich właścicieli by żyło im się razem wspanialej.</span>
-                    <span className="text3">Trenuję zgodnie z metodą <a className="lockie" href="">@Emotional Horsemanship by Lockie Philips.</a></span>
-                    <span className="text2">POMOGĘ TOBIE W STWORZENIU HARMONIJNEJ I PEŁNEJ RADOŚCI RELACJI Z TWOIM KONIEM
-            ORAZ W POCZUCIU ROZLUŹNIENIA, PEWNOŚCI SIEBIE i PRZYJEMNOŚCI Z JAZDY.
-            Zapraszam do uczestnictwa w jednym z moich programów lub na indywidualne treningi, gdzie wykorzystuję nowoczesne podejście prowadzące do ZSYNCHRONIZOWANEGO, SKUPIONEGO, POŁĄCZONEGO Z TOBĄ, SZCZĘŚLIWEGO I ZDROWEGO KONIA</span>
+                    <span className="text1">Empathy, cooperation, tolerance, acceptance, modernity and science.</span>
+                    <span className="text2">I am a trainer and coach. I support horses and their owners so that they live together better.</span>
+                    <span className="text3">I train according to the <a className="lockie" href="https://www.emotionalhorsemanship.com/">@Emotional Horsemanship by Lockie Philips.</a></span>
+                    <span className="text2">I WILL HELP YOU TO CREATE A HARMONIOUS AND FULL OF JOY RELATIONSHIP WITH YOUR HORSE AND IN A FEELING OF RELAXATION, CONFIDENCE AND PLEASURE FROM RIDING. I invite you to participate in one of my programs or for individual training, where I use a modern approach leading to a SYNCHRONIZED, FOCUSED, CONNECTED, HAPPY AND HEALTHY HORSE</span>
                     <button className="bookalesson-btn">Book a lesson</button>
                 </div>
             </section>
