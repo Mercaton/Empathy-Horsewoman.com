8c5f7933a11d0175ee44353f709e741b73d13d96
mercaton <m.wieczorek001@wp.pl>
2023-08-10 00:00:39 +0200

    trying different solutions

--- src/assets/scripts/components/Header.jsx
+++ src/assets/scripts/components/Header.jsx
@@ -1,10 +1,24 @@
 const activePage = window.location.pathname;
-const navLinks = document.querySelectorAll('ul a')
-    .forEach(link => {
-        if(link.href.includes(`${activePage}`)){
-            console.log(`${activePage}`)
-        }
-    });
+const navLinks = document.querySelectorAll('ul a');
+navLinks.forEach(link => {
+    if (link.href.includes(`${activePage}`)) {
+        link.classList.add('active');
+    }
+});
+
+// const menuLinks = document.querySelectorAll('.list-item');
+//
+// menuLinks.forEach(link => {
+//     link.addEventListener('click', (event) => {
+//         // Usuń podświetlenie ze wszystkich linków
+//         menuLinks.forEach(link => {
+//             link.classList.remove('active');
+//         });
+//
+//         // Dodaj podświetlenie do klikniętego linku
+//         event.target.classList.add('active');
+//     });
+// });
 
 function Header() {
     return (
@@ -16,7 +30,7 @@ function Header() {
                     </label>
                     <label className="logo-text">Empathy Horsewoman</label>
                     <ul className="menu-list">
-                        <li className="nav-item"><a className="list-item active" href="/">Start</a></li>
+                        <li className="nav-item"><a className="list-item" href="/">Start</a></li>
                         <li className="nav-item"><a className="list-item" href="/about-me">About me</a></li>
                         <li className="nav-item"><a className="list-item" href="/book">Book a lesson</a></li>
                         <li className="nav-item"><a className="list-item" href="/contact">Contact</a></li>
